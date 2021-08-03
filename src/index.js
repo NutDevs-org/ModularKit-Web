21b13945b8243396d5930ff7131a4c45c507bed4
Yohann Boniface <edhyjox@gmail.com>
2021-08-03 20:18:26 +0200

    adding footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './ui/main/main.scss'
 import Header from "./ui/modules/header/Header";
+import Footer from "./ui/modules/footer/Footer";
 
 ReactDOM.render(
     <React.StrictMode>
         <Header/>
+        <Footer/>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/ui/modules/footer/Footer.js
+++ src/ui/modules/footer/Footer.js
@@ -2,21 +2,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="main-footer">
-                <div className="main-footer-container">
-                    <a className="titlemain-logo" href="https://www.sunproject.xyz/">
-                        <img className="sunproj-logo" src="https://www.sunproject.xyz/src/img/logo.png" height="48px"
-                             width="48px"
-                             alt="SunProject.xyz Logo" />
-                            <p>NutDevs.org</p>
-                    </a>
+        <footer className="main-footer">
+            <div className="main-footer-container">
+                <a className="titlemain-logo" href="https://www.sunproject.xyz/">
+                    <img className="sunproj-logo" src="https://www.sunproject.xyz/src/img/logo.png" height="48px"
+                         width="48px"
+                         alt="SunProject.xyz Logo"/>
+                    <p>NutDevs.org</p>
+                </a>
 
-                    <p className="footer-about">© 2021 NutDevs.org - All right reserved.</p>
-                </div>
-            </footer>
-
-        </div>
+                <p className="footer-about">© 2021 NutDevs.org - All right reserved.</p>
+            </div>
+        </footer>
     );
 };
 

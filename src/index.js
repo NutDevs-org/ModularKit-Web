48a88fc4183387c5a0744733133132b1239b6cd5
Sundev79 <sundev79@nutdevs.org>
2021-08-09 18:58:07 +0200

    Fixed Colors FileViewer And More !

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import './ui/main/main.scss'
 
 import Header from "./ui/modules/header/Header";
-import Main from "./ui/modules/main/Main";
 import Footer from "./ui/modules/footer/Footer";
 import { initializeIcons } from '@fluentui/react/lib/Icons';
+import FileViewer from "./ui/modules/FileViewer/FileViewer";
+
+const gitBaseUrl = "https://github.com/NutDevs-org";
+const projName = "ModularKit";
+const gitProjUrl = `${gitBaseUrl}/${projName}`;
+export {gitBaseUrl, projName, gitProjUrl}
 
 initializeIcons();
 
 ReactDOM.render(
     <React.StrictMode>
         <Header/>
-        <Main/>
+        <FileViewer fileurl={`${gitProjUrl}/README.md`}/>
         <Footer/>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+
---
+++ src/ui/modules/FileViewer/FileViewer.js
@@ -0,0 +1,43 @@
+import React, {Component, useEffect} from 'react';
+import {FileCodeIcon} from "@fluentui/react-icons-mdl2";
+import "./FileViewer.scss"
+import marked from "marked";
+import hljs from "highlight.js";
+import './_hljs.scss'
+
+
+    let fileFetcher = (url) => {
+        return fetch(url).then((res) => res = res.text());
+    };
+
+
+
+    const FileViewer = (props) => {
+        let filename = props.fileurl.substr(props.fileurl.lastIndexOf("/") + 1);
+
+        useEffect(() => {
+            fileFetcher("https://raw.githubusercontent.com/NutDevs-org/ModularKit/main/README.md")
+                .then((res) => {
+                    document.querySelector(".file-content").innerHTML = marked(res);
+                    hljs.highlightAll();
+                })
+        })
+
+        return (
+            <main className="main-content">
+                <div className="main-content-container">
+                    <header className="md-head-info">
+                        <FileCodeIcon/>
+                        <a className="file-name" title="View it on Github"
+                           href={props.fileurl}
+                           target="_blank" rel="noreferrer">{filename}</a>
+                    </header>
+                    <div className="file-content"/>
+                </div>
+            </main>
+        );
+    }
+
+
+
+export default FileViewer;

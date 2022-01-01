46db2b0b9246bbd2578b55014d387654e005236c
Sundev79 <sundev79@nutdevs.org>
2022-01-01 14:04:09 +0100

    Index: Remove garbage

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./ui/main/main.scss";
-
-import Header from "./ui/modules/header/Header";
-import Footer from "./ui/modules/footer/Footer";
-import {initializeIcons} from "@fluentui/react/lib/Icons";
+import Header from "./ui/modules/Header/Header";
+import Footer from "./ui/modules/Footer/Footer";
 import FileViewer from "./ui/modules/FileViewer/FileViewer";
 
-const gitBaseUrl = "https://github.com/NutDevs-org";
-const projName = "ModularKit";
-const gitProjUrl = `${gitBaseUrl}/${projName}`;
-export {gitBaseUrl, projName, gitProjUrl};
-
-initializeIcons();
-
 ReactDOM.render(
-    <React.StrictMode>
+    <React.Fragment>
         <Header/>
-        <FileViewer fileurl={`${gitProjUrl}/README.md`}/>
+        <FileViewer/>
         <Footer/>
-    </React.StrictMode>,
+    </React.Fragment>,
     document.getElementById("root")
 );
 

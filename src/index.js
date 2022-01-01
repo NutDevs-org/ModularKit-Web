e6ff810ed87214592026116fcc029e3eca75e3b9
Sundev79 <sundev79@nutdevs.org>
2022-01-01 23:53:45 +0100

    PROJ: Added Service Workers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Common/main.scss";
-import { FileViewer, Header, Footer } from "modularkit-web/modules";
+import { FileViewer, Header, Footer } from "./Modules";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.Fragment>
@@ -11,3 +12,8 @@ ReactDOM.render(
   </React.Fragment>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();

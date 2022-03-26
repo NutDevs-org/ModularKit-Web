039e2ed666a1f2452b0900d675c383a5ecd0e6e6
Powerm1nt <sundev79@nutdevs.org>
2022-03-26 18:33:27 +0100

    FileViewer: Updated document link

--- src/Modules/FileViewer/FileViewer.jsx
+++ src/Modules/FileViewer/FileViewer.jsx
@@ -1,32 +1,37 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./FileViewer.scss";
 import { marked } from "marked";
 import hljs from "highlight.js";
 import "./_hljs.scss";
 
-
 let fileFetcher = (url) => {
-    return fetch(url).then((res) => res.text());
+  return fetch(url).then((res) => res.text());
 };
 
-
 const FileViewer = (props) => {
-    useEffect(() => {
-        fileFetcher("https://raw.githubusercontent.com/NutDevs-org/ModularKit/main/README.md")
-            .then((res) => {
-                document.querySelector(".file-content").innerHTML = marked.parse(res);
-                hljs.highlightAll();
-            })
-            .catch(() => document.querySelector(".file-content").innerHTML = "<h1>Unable to fetch the file :/</h1>");
-    });
+  useEffect(() => {
+    fileFetcher(
+      "https://raw.githubusercontent.com/NukaWorks/ModularKit/main/README.md"
+    )
+      .then((res) => {
+        document.querySelector(".file-content").innerHTML = marked.parse(res);
+        hljs.highlightAll();
+      })
+      .catch(
+        () =>
+          (document.querySelector(".file-content").innerHTML =
+            "<h1>Unable to fetch the file :/</h1>")
+      );
+  });
 
-    return (
-        <main className="main-content">
-            <div className="main-content-container">
-                <div className="file-content"/>
-            </div>
-        </main>
-    );
+  return (
+    <main className="main-content">
+      <div className="main-content-container">
+        <div className="file-content" />
+      </div>
+    </main>
+  );
 };
 
 export default FileViewer;
+

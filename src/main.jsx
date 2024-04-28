2cc2767e8ab13c1eb8db4a38a1b767b3119e8ccb
Leonard Chabot <leonard.chabot@nuka.works>
2024-04-28 16:45:17 +0200

    Removed svc_worker & updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./Common/main.scss";
 import { FileViewer, Header, Footer } from "./Modules";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+  root.render(
   <React.Fragment>
     <Header />
     <FileViewer />
     <Footer />
-  </React.Fragment>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+  </React.Fragment>
+  );

c0d8cd258c89d1cb8180bafb4c73c1ef7d885a44
powerm1nt <leonard.chabot@nuka.works>
2024-11-07 15:06:44 +0100

    Fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Common/main.scss";
 import { FileViewer, Header, Footer } from "./Modules";
+import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById("root"));
   root.render(
